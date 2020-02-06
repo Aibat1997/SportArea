e51cd3cb5d6f6df63b29ea50b4b898357d403aad
Kalibayev Nursultan <nursultan.kalibayev@kaspi.kz>
2020-02-06 14:46:28 +0600

    add mounted lifecycle add checkboxDropdown function

--- resources/js/components/AddInventory.vue
+++ resources/js/components/AddInventory.vue
@@ -1,195 +1,174 @@
 <template>
-  <div>
-    <!-- Showed -->
-    <form>
-      <div class="form-item-half d-flex-justify-start">
-        <div class="edit-cover">
-          <a
-            href="#tab-4"
-            data-toggle="tab"
-            class="btn-plain add-call invent-add"
-            @click="clearInventory"
-          >
-            <img src="/index/img/icon/plus-grey.png" />Добавить инвентар
-          </a>
-        </div>
-        <label class="date-label" v-if="inventories">
-          <div class="new-invent" v-for="inventory in inventories">
-            <div class="checkbox-cover d-flex-justify">
-              <input
-                type="checkbox"
-                class="ios8-switch checkbox-attr"
-                :id="inventory.inv_id"
-                v-model="inventory.inv_is_active"
-                @change="activateInventory(inventory)"
-              />
-              <label :for="inventory.inv_id"></label>
-              <p>
-                {{ inventory.inv_name }} -
-                <span>{{ inventory.inv_cost }}</span>
-              </p>
-              <button class="btn-plain invent-edit" @click="editInventory(inventory)">
-                <i class="icon book-edit"></i>
-              </button>
+    <div>
+        <!-- Showed -->
+        <form>
+            <div class="form-item-half d-flex-justify-start">
+                <div class="edit-cover">
+                    <a href="#tab-4" data-toggle="tab" class="btn-plain add-call invent-add" @click="clearInventory">
+                        <img src="/index/img/icon/plus-grey.png"/>Добавить инвентар
+                    </a>
+                </div>
+                <label class="date-label" v-if="inventories">
+                    <div class="new-invent" v-for="inventory in inventories">
+                        <div class="checkbox-cover d-flex-justify">
+                            <input type="checkbox" class="ios8-switch checkbox-attr" :id="inventory.inv_id" v-model="inventory.inv_is_active" @change="activateInventory(inventory)"/>
+                            <label :for="inventory.inv_id"></label>
+                            <p>
+                                {{ inventory.inv_name }} -
+                                <span>{{ inventory.inv_cost }}</span>
+                            </p>
+                            <button class="btn-plain invent-edit" @click="editInventory(inventory)">
+                                <i class="icon book-edit"></i>
+                            </button>
+                        </div>
+                    </div>
+                </label>
             </div>
-          </div>
-        </label>
-      </div>
-    </form>
+        </form>
 
-    <!-- Don't showed -->
-    <div class="question-popup add-invent-modal">
-      <div class="question-popup-head d-flex-justify">
-        <h3>Добавить инвентар</h3>
-        <button class="btn-plain close-popup">
-          <i class="fas fa-times"></i>
-        </button>
-      </div>
-      <form>
-        <div class="error-box" v-if="errors">
-          <ul>
-            <li v-for="(key,error) in errors">{{error}} : {{ key[0] }}</li>
-          </ul>
+        <!-- Don't showed -->
+        <div class="question-popup add-invent-modal">
+            <div class="question-popup-head d-flex-justify">
+                <h3>Добавить инвентар</h3>
+                <button class="btn-plain close-popup">
+                    <i class="fas fa-times"></i>
+                </button>
+            </div>
+            <form>
+                <div class="error-box" v-if="errors">
+                    <ul>
+                        <li v-for="(key,error) in errors">{{error}} : {{ key[0] }}</li>
+                    </ul>
+                </div>
+                <div class="error-box success-box" v-if="seccsess_msg">{{ seccsess_msg }}</div>
+                <div class="inventar-label d-flex">
+                    <input type="text" placeholder="Название" v-model="inventory.inv_name" class="input-attr invent-text"/>
+                    <label class="price-current">
+                        <input type="number" placeholder="Цена" v-model="inventory.inv_cost" class="input-attr"/>
+                        <i class="icon i-tg"></i>
+                    </label>
+                </div>
+                <button class="btn-plain btn-blue" @click.prevent="sendNew">Сохранить</button>
+            </form>
         </div>
-        <div class="error-box success-box" v-if="seccsess_msg">{{ seccsess_msg }}</div>
-        <div class="inventar-label d-flex">
-          <input
-            type="text"
-            placeholder="Название"
-            v-model="inventory.inv_name"
-            class="input-attr invent-text"
-          />
-          <label class="price-current">
-            <input type="number" placeholder="Цена" v-model="inventory.inv_cost" class="input-attr" />
-            <i class="icon i-tg"></i>
-          </label>
+        <div class="question-popup edit-invent-modal">
+            <div class="question-popup-head d-flex-justify">
+                <h3>Редактировать инвентар</h3>
+                <button class="btn-plain close-popup">
+                    <i class="fas fa-times"></i>
+                </button>
+            </div>
+            <form>
+                <div class="error-box" v-if="errors">
+                    <ul>
+                        <li v-for="(key,error) in errors">{{error}} : {{ key[0] }}</li>
+                    </ul>
+                </div>
+                <div class="error-box success-box" v-if="seccsess_msg">{{ seccsess_msg }}</div>
+                <div class="inventar-label d-flex">
+                    <div class="checkbox-cover">
+                        <input type="checkbox" class="ios8-switch checkbox-attr" v-model="inventory.inv_is_active" id="checkbox-106"/>
+                        <label for="checkbox-106"></label>
+                        <input type="text" placeholder="Инвентар" v-model="inventory.inv_name" class="input-attr"/>
+                    </div>
+                    <label class="price-current">
+                        <input type="text" placeholder="Цена" v-model="inventory.inv_cost" class="input-attr"/>
+                        <i class="icon i-tg"></i>
+                    </label>
+                </div>
+                <div class="inventar-label d-flex">
+                    <button class="btn-plain btn-blue" @click.prevent="updateCurrentInventory">Сохранить</button>
+                </div>
+            </form>
         </div>
-        <button class="btn-plain btn-blue" @click.prevent="sendNew">Сохранить</button>
-      </form>
+        <div class="overlay"></div>
     </div>
-    <div class="question-popup edit-invent-modal">
-      <div class="question-popup-head d-flex-justify">
-        <h3>Редактировать инвентар</h3>
-        <button class="btn-plain close-popup">
-          <i class="fas fa-times"></i>
-        </button>
-      </div>
-      <form>
-        <div class="error-box" v-if="errors">
-          <ul>
-            <li v-for="(key,error) in errors">{{error}} : {{ key[0] }}</li>
-          </ul>
-        </div>
-        <div class="error-box success-box" v-if="seccsess_msg">{{ seccsess_msg }}</div>
-        <div class="inventar-label d-flex">
-          <div class="checkbox-cover">
-            <input
-              type="checkbox"
-              class="ios8-switch checkbox-attr"
-              v-model="inventory.inv_is_active"
-              id="checkbox-106"
-            />
-            <label for="checkbox-106"></label>
-            <input
-              type="text"
-              placeholder="Инвентар"
-              v-model="inventory.inv_name"
-              class="input-attr"
-            />
-          </div>
-          <label class="price-current">
-            <input type="text" placeholder="Цена" v-model="inventory.inv_cost" class="input-attr" />
-            <i class="icon i-tg"></i>
-          </label>
-        </div>
-        <div class="inventar-label d-flex">
-          <button class="btn-plain btn-blue" @click.prevent="updateCurrentInventory">Сохранить</button>
-        </div>
-      </form>
-    </div>
-    <div class="overlay"></div>
-  </div>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      inventories: [],
-      errors: null,
-      seccsess_msg: "",
-      inventory: {
-        inv_id: null,
-        inv_name: "",
-        inv_cost: 0,
-        inv_is_active: false
-      }
-    };
-  },
-  created() {
-    axios.get("/inventory").then(response => {
-      this.inventories = response.data;
-    });
-  },
-  methods: {
-    clearInventory() {
-      this.inventory = {};
-      this.seccsess_msg = "";
-      this.errors = null;
-    },
-    editInventory(element) {
-      this.inventory = element;
-      this.seccsess_msg = "";
-      this.errors = null;
-    },
-    sendNew() {
-      this.postInventory("/store-inventory", this.inventory);
-    },
-    activateInventory(inventory) {
-      this.postInventory("/update-inventory", inventory);
-    },
-    updateCurrentInventory() {
-      this.postInventory("/update-inventory", this.inventory);
-    },
-    postInventory(url, value) {
-      axios
-        .post(url, value)
-        .then(response => {
-          this.inventories = response.data.content;
-          this.seccsess_msg = response.data.message;
-          this.errors = null;
-        })
-        .catch(error => {
-          this.seccsess_msg = "";
-          this.errors = error.response.data.errors;
-        });
+    export default {
+        data () {
+            return {
+                inventories: [],
+                errors: null,
+                seccsess_msg: '',
+                inventory: {
+                    inv_id: null,
+                    inv_name: '',
+                    inv_cost: 0,
+                    inv_is_active: false,
+                },
+            }
+        },
+        created () {
+            axios.get('/inventory').then(response => {
+                this.inventories = response.data
+            })
+        },
+        mounted: function () {
+            if (typeof checkboxDropdown === 'function') {
+                checkboxDropdown('.dropdown')
+            }
+        },
+        methods: {
+            clearInventory () {
+                this.inventory = {}
+                this.seccsess_msg = ''
+                this.errors = null
+            },
+            editInventory (element) {
+                this.inventory = element
+                this.seccsess_msg = ''
+                this.errors = null
+            },
+            sendNew () {
+                this.postInventory('/store-inventory', this.inventory)
+            },
+            activateInventory (inventory) {
+                this.postInventory('/update-inventory', inventory)
+            },
+            updateCurrentInventory () {
+                this.postInventory('/update-inventory', this.inventory)
+            },
+            postInventory (url, value) {
+                axios.post(url, value).then(response => {
+                    this.inventories = response.data.content
+                    this.seccsess_msg = response.data.message
+                    this.errors = null
+                }).catch(error => {
+                    this.seccsess_msg = ''
+                    this.errors = error.response.data.errors
+                })
+            },
+        },
     }
-  }
-};
 </script>
 <style>
-.error-box {
-  background-color: #e43027;
-  color: white;
-  padding: 15px;
-  border-radius: 5px;
-  margin-bottom: 20px;
-}
-.error-box ul li {
-  color: white;
-  font-size: 14px;
-  margin-bottom: 3px;
-}
-.edit-invent-modal {
-  left: 0;
-}
-.edit-invent-modal .checkbox-cover {
-  background: #ffffff;
-  border: none;
-  -webkit-border-radius: 3px;
-  border-radius: 3px;
-  margin-bottom: 0;
-  padding: 0;
-  width: 49%;
-}
-</style>   
+    .error-box {
+        background-color: #e43027;
+        color: white;
+        padding: 15px;
+        border-radius: 5px;
+        margin-bottom: 20px;
+    }
+
+    .error-box ul li {
+        color: white;
+        font-size: 14px;
+        margin-bottom: 3px;
+    }
+
+    .edit-invent-modal {
+        left: 0;
+    }
+
+    .edit-invent-modal .checkbox-cover {
+        background: #ffffff;
+        border: none;
+        -webkit-border-radius: 3px;
+        border-radius: 3px;
+        margin-bottom: 0;
+        padding: 0;
+        width: 49%;
+    }
+</style>
--- resources/views/index/complex-create.blade.php
+++ resources/views/index/complex-create.blade.php
@@ -1,111 +1,109 @@
 @extends('index.layouts.layout')
 
 @section('content')
-<section class="search-result">
-    <div class="container">
-        <h1>Добавление объекта</h1>
-    </div>
-</section>
-<section class="main add-object">
-    <div class="container">
-        <div class="add-object-cover">
-            <div class="row">
-                <add-complex></add-complex>
-                <complex-information></complex-information>
+    <section class="search-result">
+        <div class="container">
+            <h1>Добавление объекта</h1>
+        </div>
+    </section>
+    <section class="main add-object">
+        <div class="container">
+            <div class="add-object-cover">
+                <div class="row">
+                    <add-complex></add-complex>
+                    <complex-information></complex-information>
+                </div>
             </div>
         </div>
-    </div>
-</section>
+    </section>
 @endsection
 @section('js')
-<script>
-    $('.select').on('click', function () {
-        $(this).find('.arrow-select').toggleClass('arrow-up')
-    });
-    $('.favorite').on('click', function () {
-        $(this).find('.i-star').toggleClass('i-star-red')
-    });
-    $('.size-mask').inputmask("99X99");
-    
-    function checkboxDropdown(el) {
-        var $el = $(el);
+    <script>
+        $('.select').on('click', function () {
+            $(this).find('.arrow-select').toggleClass('arrow-up')
+        })
+        $('.favorite').on('click', function () {
+            $(this).find('.i-star').toggleClass('i-star-red')
+        })
+        $('.size-mask').inputmask('99X99')
 
-        function updateStatus(label, result) {
-            if (!result.length) {
-                label.html('Инфраструктура');
-            }
-        }
+        function checkboxDropdown (el) {
+            var $el = $(el)
 
-        ;
-        $el.each(function (i, element) {
-            var _this = this;
-
-            var $list = $(this).find('.dropdown-list'),
-                $label = $(this).find('.dropdown-label'),
-                $checkAll = $(this).find('.check-all'),
-                $inputs = $(this).find('.check'),
-                defaultChecked = $(this).find('input[type=checkbox]:checked'),
-                result = [];
-            updateStatus($label, result);
-
-            if (defaultChecked.length) {
-                defaultChecked.each(function () {
-                    result.push($(this).next().text());
-                    $label.html(result.join(", "));
-                });
+            function updateStatus (label, result) {
+                if (!result.length) {
+                    label.html('Инфраструктура')
+                }
             }
 
-            $label.on('click', function () {
-                $(_this).toggleClass('open');
-            });
-            $checkAll.on('change', function () {
-                var checked = $(this).is(':checked');
-                var checkedText = $(this).next().text();
-                result = [];
+            $el.each(function (i, element) {
+                var _this = this
+                var $list = $(this).find('.dropdown-list'),
+                    $label = $(this).find('.dropdown-label'),
+                    $checkAll = $(this).find('.check-all'),
+                    $inputs = $(this).find('.check'),
+                    defaultChecked = $(this).find('input[type=checkbox]:checked'),
+                    result = []
+                updateStatus($label, result)
 
-                if (checked) {
-                    result.push(checkedText);
-                    $label.html(result);
-                    $inputs.prop('checked', false);
-                } else {
-                    $label.html(result);
+                if (defaultChecked.length) {
+                    defaultChecked.each(function () {
+                        result.push($(this).next().text())
+                        $label.html(result.join(', '))
+                    })
                 }
 
-                updateStatus($label, result);
-            });
-            $inputs.on('change', function () {
-                var checked = $(this).is(':checked');
-                var checkedText = $(this).next().text();
+                $label.on('click', function () {
+                    $(_this).toggleClass('open')
+                })
+                $checkAll.on('change', function () {
+                    var checked = $(this).is(':checked')
+                    var checkedText = $(this).next().text()
+                    result = []
 
-                if ($checkAll.is(':checked')) {
-                    result = [];
-                }
-
-                if (checked) {
-                    result.push(checkedText);
-                    $label.html(result.join(", "));
-                    $checkAll.prop('checked', false);
-                } else {
-                    var index = result.indexOf(checkedText);
-
-                    if (index >= 0) {
-                        result.splice(index, 1);
+                    if (checked) {
+                        result.push(checkedText)
+                        $label.html(result)
+                        $inputs.prop('checked', false)
+                    } else {
+                        $label.html(result)
                     }
 
-                    $label.html(result.join(", "));
-                }
+                    updateStatus($label, result)
+                })
+                $inputs.on('change', function () {
+                    var checked = $(this).is(':checked')
+                    var checkedText = $(this).next().text()
 
-                updateStatus($label, result);
-            });
-            $(document).on('click touchstart', function (e) {
-                if (!$(e.target).closest($(_this)).length) {
-                    $(_this).removeClass('open');
-                }
-            });
-        });
-    }
+                    if ($checkAll.is(':checked')) {
+                        result = []
+                    }
 
-    checkboxDropdown('.dropdown');
+                    if (checked) {
+                        result.push(checkedText)
+                        $label.html(result.join(', '))
+                        $checkAll.prop('checked', false)
+                    } else {
+                        var index = result.indexOf(checkedText)
 
-</script>
+                        if (index >= 0) {
+                            result.splice(index, 1)
+                        }
+
+                        $label.html(result.join(', '))
+                    }
+
+                    updateStatus($label, result)
+                })
+                $(document).on('click touchstart', function (e) {
+                    if (!$(e.target).closest($(_this)).length) {
+                        $(_this).removeClass('open')
+                    }
+                })
+            })
+        }
+
+        checkboxDropdown('.dropdown')
+
+    </script>
 @endsection
